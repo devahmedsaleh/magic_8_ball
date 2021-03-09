b72766e6d7cabba07fe6c0363197f97f376dacb1
Ahmed Saleh <>
2021-03-10 00:24:48 +0200

    add app theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,23 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(
+    MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.blue.shade900,
+        appBarTheme: AppBarTheme(
+          centerTitle: true,
+        ),
+        scaffoldBackgroundColor: Colors.blue,
+      ),
+      home: BallPage(),
+    ),
+  );
+}
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
